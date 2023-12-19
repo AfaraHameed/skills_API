601e9933efcd49a51346ddd0bf74c84cac7804c2
AfaraHameed <afarasidheek@gmail.com>
2023-12-19 18:15:43 +0530

    created hashed password and stored to db

--- repositories/users.js
+++ repositories/users.js
@@ -1,9 +1,11 @@
 const usersQueries = require("../queries/users");
 const pool = require("../config/db");
+const {hashPassword} = require('../utils/passwordHelper');
 
 addUser = (name,username,password)=>{
+    const hashedPassword = hashPassword(password)
     return new Promise((resolve,reject)=>{
-      pool.query(usersQueries.createUser, [name,username,password], (error, result) => {
+      pool.query(usersQueries.createUser, [name,username,hashedPassword], (error, result) => {
         if(error){
           reject(error)
         }
